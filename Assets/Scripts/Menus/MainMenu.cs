e51342f1a6828946b98010d1a58d8fc02c906208
Michael MacLean <20232905+hanabanashiku@users.noreply.github.com>
2022-09-30 23:24:43 -0400

    Initial solution

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace Hanabanashiku.GameJam.Menus {
+    public class MainMenu : MonoBehaviour {
+        public void ExitGame() {
+            Console.WriteLine("test");
+            Application.Quit();
+        }
+    }
+}
+
